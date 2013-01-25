d099a10799e17dce59b7bd703b444f82cb7ca725
DucCao <condorhero01@gmail.com>
2013-01-25 11:51:09 +0700

    getting current location and add a marker

--- src/com/condorhero89/policescanner/MainActivity.java
+++ src/com/condorhero89/policescanner/MainActivity.java
@@ -1,21 +1,27 @@
 package com.condorhero89.policescanner;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
+    private LocationManager mLocationManager;
+    private Location mCurrentLocation;
+    private LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +29,51 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         getFragmentManager().findFragmentById(R.id.map);
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                .title("Hamburg"));
-        Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.ic_launcher)));
-
-        // Move the camera instantly to hamburg with a zoom of 15.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-
-        // Zoom in, animating the camera.
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        
+        retrieveCurrentLocation();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        
+        Log.i("LocationManager", "removeUpdates");
+        mLocationManager.removeUpdates(mLocationListener);
+    }
+
+    private void retrieveCurrentLocation() {
+        mLocationListener = new LocationListener() {
+            
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+            
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+            
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+            
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("LocationListener", "onLocationChanged: " + location);
+                LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
+                
+                map.addMarker(new MarkerOptions()
+                      .position(currentLatLng)
+                      .title("current location")
+                      .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+                
+                mCurrentLocation = location;
+            }
+        };
+        
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+    }
 }
